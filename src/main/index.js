014b7296799d62b8c0b3dfbeaab8685c997e71bb
Luis Float <contact@luisfloat.com>
2022-07-12 14:46:32 -0300

    feat(index): init

---
+++ src/main/index.js
@@ -0,0 +1,17 @@
+import octopo from "./octopo.js";
+import * as builder from "./builder.js";
+import * as langs from "./langs/index.js";
+import * as logger from "./logger.js";
+import * as stringify from "./stringify.js";
+import * as stringifyFile from "./stringifyFile.js";
+
+export default octopo;
+
+export {
+    octopo,
+    builder,
+    langs,
+    logger,
+    stringify,
+    stringifyFile,
+};
