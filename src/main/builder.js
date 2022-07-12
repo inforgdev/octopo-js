81d34c324f9537a7762a0f65ecb1f68d54d0a974
Luis Float <contact@luisfloat.com>
2022-07-11 22:05:46 -0300

    feat(builder): add arg

--- src/main/builder.js
+++ src/main/builder.js
@@ -1,7 +1,14 @@
 export function call(name, args) {
-        return {
-            "type": "call",
-            "name": name,
-            "args": args,
-        };
+    return {
+        "type": "call",
+        "name": name,
+        "args": args,
+    };
+}
+
+export function arg(expression) {
+    return {
+        "type": "arg",
+        "expression": expression,
+    };
 }
