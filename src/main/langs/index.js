c65d774497cccb4ca1e51518fff744003185f8c8
Honor <admin@honor2045.com>
2022-07-11 23:01:59 -0300

    feat(langs): add index file

---
+++ src/main/langs/index.js
@@ -0,0 +1,11 @@
+import less from "./less.js";
+import lessMin from "./lessMin.js";
+import scss from "./scss.js";
+import styl from "./styl.js";
+
+export {
+    less,
+    lessMin,
+    scss,
+    styl,
+};
