eea5ec95229c9d6d7a7cf41c27799caa48b4a930
Honor <admin@honor2045.com>
2022-07-11 23:00:33 -0300

    feat(langs): add styl

---
+++ src/main/langs/styl.js
@@ -0,0 +1,23 @@
+export default {
+    extname: ".styl",
+
+    wordSep: "-",
+
+    statementEnd: ";",
+    needLastStatementEnd: true,
+    needBrln: true,
+    v: "_NAME_ = _EXPRESSION_",
+    dec: "_NAME_: _EXPRESSION_",
+    call: "._NAME_(_ARGS_)",
+    argSep: ", ",
+    arg: "",
+
+    ref: "_NAME_",
+    val: "_NAME_",
+
+    indent: "  ",
+    mixin: "_NAME_(_PARAMS_) {\n_BODY_}",
+    param: "_NAME_",
+    paramDefault: "_NAME_ = _DEFAULT_",
+    paramSep: ", ",
+};
