32edfb743e482ce49ac93c3ef6ce94c3e41e8f0b
Honor <admin@honor2045.com>
2022-07-11 22:59:27 -0300

    feat(langs): add scss

---
+++ src/main/langs/scss.js
@@ -0,0 +1,23 @@
+export default {
+    extname: ".scss",
+
+    wordSep: "-",
+
+    statementEnd: ";",
+    needLastStatementEnd: true,
+    needBrln: true,
+    v: "$_NAME_: _EXPRESSION_",
+    dec: "_NAME_: _EXPRESSION_",
+    call: "@include _NAME_(_ARGS_)",
+    argSep: ", ",
+    arg: "",
+
+    ref: "$_NAME_",
+    val: "_NAME_",
+
+    indent: "  ",
+    mixin: "@mixin _NAME_(_PARAMS_) {\n_BODY_}",
+    param: "$_NAME_",
+    paramDefault: "$_NAME_: _DEFAULT_",
+    paramSep: ", ",
+};
