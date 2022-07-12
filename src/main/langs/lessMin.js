0e6f524601c12e36da39934ea2b25493c9acd83c
Luis Float <contact@luisfloat.com>
2022-07-11 23:01:31 -0300

    feat(langs): add less minified

---
+++ src/main/langs/lessMin.js
@@ -0,0 +1,23 @@
+export default {
+    extname: ".less",
+    
+    wordSep: "-",
+    
+    statementEnd: ";",
+    needLastStatementEnd: false,
+    needBrln: false,
+    v: "@_NAME_:_EXPRESSION_",
+    dec: "_PROP_:_VALUE_",
+    call: "._NAME_(_ARGS_)",
+    argSep: ",",
+    arg: "",
+
+    ref: "@_NAME_",
+    val: "_NAME_",
+
+    indent: "",
+    mixin: "._NAME_(_PARAMS_){_BODY_}",
+    param: "@_NAME_",
+    paramDefault: "@_NAME_:_DEFAULT_",
+    paramSep: ",",
+};
