db7faeb32cef97bd028755d18dae14bb09a2bea1
Honor <admin@honor2045.com>
2022-07-12 00:44:58 -0300

    feat(stringifyFile): add 'stringifyFile'

--- src/main/stringifyFile.js
+++ src/main/stringifyFile.js
@@ -1,9 +1,15 @@
 import { writeFileSync, mkdirSync } from "fs";
 import { join, dirname } from "path";
+import stringify from "./stringify.js";
 
 export function writeFileResolve(data, options) {
     const fileDist = join(options.dirname, options.name + options.extname);
     const targetDir = dirname(fileDist);
     mkdirSync(targetDir, { recursive: true });
     writeFileSync(fileDist, data);
+}
+
+export default function stringifyFile(options) {
+    const data = stringify(options);
+    writeFileResolve(data, options.out.file);
 }
