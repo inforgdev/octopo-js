71d42a2fb09bd34683d6e0b0e6584ec45ea7f91c
Honor <admin@honor2045.com>
2022-07-13 17:03:38 -0300

    feat(stringify): add lang parameter with 'data' as default

--- src/main/stringify.js
+++ src/main/stringify.js
@@ -9,119 +9,119 @@ export default function stringify(options) {
     if(ast === undefined) return propUndefined("options.in.data");
     if(data === undefined) return propUndefined("options.proc.data");
 
-    return globalStatements(ast);
+    return globalStatements(ast, data);
 }
 
-export function ref(node) {
+export function ref(node, lang = data) {
     if(node.type !== "ref") return unexpected(node);
-    return data.ref.replaceAll("_NAME_", node.name);
+    return lang.ref.replaceAll("_NAME_", node.name);
 }
 
-export function val(node) {
+export function val(node, lang = data) {
     if(node.type !== "val") return unexpected(node);
-    return data.val.replaceAll("_NAME_", node.expression);
+    return lang.val.replaceAll("_NAME_", node.expression);
 }
 
-export function exp(node) {
+export function exp(node, lang = data) {
     if(node === undefined) return unexpected(node);
 
     switch(node.type) {
         case "ref":
-            return ref(node);
+            return ref(node, lang);
         case "val":
-            return val(node);
+            return val(node, lang);
         default:
             return unexpected(node);
     }
 }
 
-export function v(node) {
+export function v(node, lang = data) {
     if(node.type !== "v") return unexpected(node);
     
-    let expressionC = exp(node.expression);
+    let expressionC = exp(node.expression, lang);
 
-    return data.v
+    return lang.var
         .replaceAll("_NAME_", node.name)
         .replaceAll("_EXPRESSION_", expressionC);
 }
 
-export function dec(node) {
+export function dec(node, lang = data) {
     if(node.type !== "dec") return unexpected(node);
 
-    let valueC = exp(node.value);
+    let valueC = exp(node.value, lang);
 
-    return data.dec
+    return lang.dec
         .replaceAll("_PROP_", node.prop)
         .replaceAll("_VALUE_", valueC);
 }
 
-export function param(node) {
+export function param(node, lang = data) {
     if(node.type !== "param") return unexpected(node);
 
     if(node.default) {
-        return data.paramDefault
+        return lang.paramDefault
             .replaceAll("_NAME_", node.name)
             .replaceAll("_DEFAULT_", node.default);
     }
 
-    return data.param.replaceAll("_NAME_", node.name);
+    return lang.param.replaceAll("_NAME_", node.name);
 }
 
-export function params(exp) {
+export function params(exp, lang = data) {
     let a = [];
 
     if(!Array.isArray(exp)) {
-        a.push(param(exp));
+        a.push(param(exp, lang));
     } else {
         for(let i in exp) {
-            a.push(param(exp[i]));
+            a.push(param(exp[i], lang));
         }
     }
 
-    return a.join(data.paramSep);
+    return a.join(lang.paramSep);
 }
 
-export function arg(node) {
+export function arg(node, lang = data) {
     if(node.type !== "arg") return unexpected(node);
-    return exp(node.expression);
+    return exp(node.expression, lang);
 }
 
-export function args(exp) {
+export function args(exp, lang = data) {
     let a = [];
 
     if(!Array.isArray(exp)) {
-        a.push(arg(exp));
+        a.push(arg(exp, lang));
     } else {
         for(let i in exp) {
-            a.push(arg(exp[i]));
+            a.push(arg(exp[i], lang));
         }
     }
 
-    return a.join(data.argSep);
+    return a.join(lang.argSep);
 }
 
-export function call(node) {
+export function call(node, lang = data) {
     if(node.type !== "call") return unexpected(node);
 
-    return data.call
+    return lang.call
         .replace("_NAME_", node.name)
-        .replaceAll("_ARGS_", args(node.args));
+        .replaceAll("_ARGS_", args(node.args, lang));
 }
 
-export function mixin(node) {
+export function mixin(node, lang = data) {
     if(node.type !== "mixin") return unexpected(node);
 
-    let mixinC = data.mixin
+    let mixinC = lang.mixin
         .replaceAll("_NAME_", node.name)
-        .replaceAll("_PARAMS_", params(node.params))
-        .replaceAll("_BODY_", body(node.body));
+        .replaceAll("_PARAMS_", params(node.params, lang))
+        .replaceAll("_BODY_", body(node.body, lang));
 
-    if(data.needBrln) mixinC = mixinC + "\n";
+    if(lang.needBrln) mixinC = mixinC + "\n";
 
     return mixinC;
 }
 
-export function body(node) {
+export function body(node, lang = data) {
     let body = node;
     let bodyC = "";
 
@@ -132,32 +132,32 @@ export function body(node) {
 
         switch(statement.type) {
             case "call":
-                sta = call(statement);
+                sta = call(statement, lang);
                 break;
             case "v":
-                sta = v(statement);
+                sta = v(statement, lang);
                 break;
             case "dec":
-                sta = dec(statement);
+                sta = dec(statement, lang);
                 break;
             case "mixin":
-                sta = mixin(statement);
+                sta = mixin(statement, lang);
                 break;
             default:
                 return unexpected(statement);
         }
 
-        let ender = data.statementEnd;
-        if(i == body.length - 1 && !data.needLastStatementEnd) ender = "";
-        if(data.needBrln) ender = ender + "\n";
+        let ender = lang.statementEnd;
+        if(i == body.length - 1 && !lang.needLastStatementEnd) ender = "";
+        if(lang.needBrln) ender = ender + "\n";
 
-        bodyC = bodyC + data.indent + sta + ender;
+        bodyC = bodyC + lang.indent + sta + ender;
     }
 
     return bodyC;
 }
 
-export function globalStatements(ast) {
+export function globalStatements(ast, lang = data) {
     let rootC = "";
 
     for(let i in ast) {
@@ -167,21 +167,21 @@ export function globalStatements(ast) {
 
         switch(node.type) {
             case "call":
-                sta = call(node);
+                sta = call(node, lang);
                 break;
             case "v":
-                sta = v(node);
+                sta = v(node, lang);
                 break;
             case "mixin":
-                sta = mixin(node);
+                sta = mixin(node, lang);
                 break;
             default:
                 return unexpected(node);
         }
 
-        let ender = data.statementEnd;
-        if(i == ast.length - 1 && !data.needLastStatementEnd || node.type === "mixin") ender = "";
-        if(data.needBrln && node.type !== "mixin") ender = ender + "\n";
+        let ender = lang.statementEnd;
+        if(i == ast.length - 1 && !lang.needLastStatementEnd || node.type === "mixin") ender = "";
+        if(lang.needBrln && node.type !== "mixin") ender = ender + "\n";
 
         rootC = rootC + sta + ender;
     }
