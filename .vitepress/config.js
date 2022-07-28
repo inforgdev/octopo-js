bad7d5b21679b830303ce351c3866cd0f7d9c65c
Luis Float <contact@luisfloat.com>
2022-07-28 17:26:46 -0300

    feat(build): add docs using vitepress

---
+++ .vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+    title: 'OctopoJS',
+    description: 'OctopoJS Website',
+    base: '/octopo-js/',
+    srcDir: './src/docs',
+    outDir: './dist/docs',
+    themeConfig: {
+        siteTitle: 'OctopoJS',
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/inforgdev/octopo-js' },
+        ],
+        footer: {
+            copyright: "By Inforg",
+        },
+    },
+}
