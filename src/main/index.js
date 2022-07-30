45788488b954d948063dede15419d937f5309e93
Luis Float <contact@luisfloat.com>
2022-07-30 17:37:20 -0300

    feat(main): add options to `index.js`

--- src/main/index.js
+++ src/main/index.js
@@ -4,6 +4,10 @@ import * as langs from "./langs/index.js";
 import * as logger from "./logger.js";
 import * as stringify from "./methods/stringify.js";
 import * as stringifyFile from "./methods/stringifyFile.js";
+import outFile from "./options/outFile.js";
+const options = {
+    outFile,
+};
 
 export default octopo;
 
@@ -14,4 +18,5 @@ export {
     logger,
     stringify,
     stringifyFile,
+    options,
 };
