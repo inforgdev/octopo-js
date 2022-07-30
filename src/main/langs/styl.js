12d54cea624e0a7a842496c1ca95b6b1bc62b334
Luis Float <contact@luisfloat.com>
2022-07-30 16:31:12 -0300

    refactor(main): comment lang keys

--- src/main/langs/styl.js
+++ src/main/langs/styl.js
@@ -1,23 +1,30 @@
 export default {
-    extname: ".styl",
+    //declaration
 
-    wordSep: "-",
+    var: "_NAME_ = _EXPRESSION_",
+    mixin: "_NAME_(_PARAMS_) {\n_BODY_}",
+    dec: "_PROP_: _VALUE_",
+    param: "_NAME_",
+    paramDefault: "_NAME_ = _DEFAULT_",
+
+    //expression
+
+    call: "._NAME_(_ARGS_)",
+    ref: "_NAME_",
+    val: "_NAME_",
+    arg: "",
+
+    //artifacts
+    
+    argSep: ", ",
+    paramSep: ", ",
+
+    //style
 
     statementEnd: ";",
     needLastStatementEnd: true,
-    needBrln: true,
-    var: "_NAME_ = _EXPRESSION_",
-    dec: "_PROP_: _VALUE_",
-    call: "._NAME_(_ARGS_)",
-    argSep: ", ",
-    arg: "",
-
-    ref: "_NAME_",
-    val: "_NAME_",
-
     indent: "  ",
-    mixin: "_NAME_(_PARAMS_) {\n_BODY_}",
-    param: "_NAME_",
-    paramDefault: "_NAME_ = _DEFAULT_",
-    paramSep: ", ",
+    needBrln: true,
+    wordSep: "-",
+    extname: ".styl",
 };
