92dd5241f8fe58a946d9ab8fb2b0d7fdb5c37115
Luis Float <contact@luisfloat.com>
2022-07-29 21:59:22 -0300

    refactor(main): move method files to `/methods/`

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,9 @@
-import octopo from "./octopo.js";
+import octopo from "./methods/octopo.js";
 import * as factory from "./factory.js";
 import * as langs from "./langs/index.js";
 import * as logger from "./logger.js";
-import * as stringify from "./stringify.js";
-import * as stringifyFile from "./stringifyFile.js";
+import * as stringify from "./methods/stringify.js";
+import * as stringifyFile from "./methods/stringifyFile.js";
 
 export default octopo;
 
