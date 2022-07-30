6e042ead1dee21c4f537fbfd9b77e0a7f97c8c94
Luis Float <contact@luisfloat.com>
2022-07-29 23:44:55 -0300

    refactor(main): move down the export default

--- src/main/methods/stringifyFile.js
+++ src/main/methods/stringifyFile.js
@@ -2,18 +2,18 @@ import { writeFileSync, mkdirSync } from "fs";
 import { join, dirname } from "path";
 import stringify from "./stringify.js";
 
+export function stringifyFile(options) {
+    const data = stringify(options);
+    writeFileResolve(data, options.out.file);
+}
+
+export default stringifyFile;
+
 export function writeFileResolve(data, options) {
     const fileDist = join(options.dirname, options.name + options.extname);
     const targetDir = dirname(fileDist);
     mkdirSync(targetDir, { recursive: true, });
     writeFileSync(fileDist, data);
-}
-
-export default stringifyFile;
-
-export function stringifyFile(options) {
-    const data = stringify(options);
-    writeFileResolve(data, options.out.file);
 }
 
 export function handleWriteFile(data, options) {
