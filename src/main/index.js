dedca410f81b10d0769bffb043851a4da9e96994
Luis Float <contact@luisfloat.com>
2022-08-21 20:18:29 -0300

    refactor(main): move `nodes/stringifyProc.js` to `./stringifyProc.js`

--- src/main/index.js
+++ src/main/index.js
@@ -2,7 +2,7 @@ import * as factory from "./nodes/factory.js";
 import * as stringifier from "./nodes/stringifier.js";
 import * as langs from "./langs/index.js";
 import * as logger from "./logger.js";
-import stringifyProc from "./nodes/stringifyProc.js";
+import stringifyProc from "./stringifyProc.js";
 
 export default stringifyProc;
 
