1b7a1cf7e867ccd1f62033de4884d7e3c9f87d18
Luis Float <contact@luisfloat.com>
2022-08-23 16:02:59 -0300

    feat(main): add scss minified

--- src/main/langs/index.js
+++ src/main/langs/index.js
@@ -1,11 +1,13 @@
 import less from "./less.js";
 import lessMin from "./lessMin.js";
 import scss from "./scss.js";
+import scssMin from "./scssMin.js";
 import styl from "./styl.js";
 
 export {
     less,
     lessMin,
     scss,
+    scssMin,
     styl,
 };
