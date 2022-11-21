671da14f2d12ff78169d6821ea5f98160d985064
Luis Float <contact@luisfloat.com>
2022-11-20 21:40:04 -0300

    feat(docs): set dark theme as default

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -3,6 +3,7 @@ import themeConfig from "../src/docs/themeConfig.js";
 export default {
     title: 'OctopoJS',
     description: 'OctopoJS Website',
+    appearance: 'dark',
     base: '/octopo-js/',
     srcDir: './src/docs/views/',
     outDir: './dist/docs',
