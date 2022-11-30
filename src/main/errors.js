536ec7a249001654c08cd0740ad921ef0e602501
Luis Float <contact@luisfloat.com>
2022-11-30 00:46:07 -0300

    refactor(main): rem unused files

--- src/main/errors.js
+++
@@ -1,9 +0,0 @@
-export function unexpected(node)  {
-    console.log("Illegal argument exception: " + JSON.stringify(node, null, 2));
-    return undefined;
-}
-
-export function propUndefined(propName)  {
-    console.log(`${propName} is undefined`);
-    return undefined;
-}
--- src/main/stringifyProc.js
+++
@@ -1,7 +0,0 @@
-import { $globalStatements } from "./nodes/stringifier.js";
-
-function stringifyProc(data, meta) {
-    return $globalStatements(data, meta.grammar);
-}
-
-export default stringifyProc;
